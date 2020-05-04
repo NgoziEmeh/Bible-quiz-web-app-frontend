0231b4d044873cbd4793cb7ce9832020cc6a6220
NgoziEmeh <zingo1ng@yahoo.com>
2020-05-04 18:55:50 +0000

    endpage component decorator

---
+++ src/app/endpage/endpage.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-endpage',
+    templateUrl: './endpage.component.html',
+    styleUrls: ['./endpage.component,css']
+})
+export class EndpageComponent {
+
+}
