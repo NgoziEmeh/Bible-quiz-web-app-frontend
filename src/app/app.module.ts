5502f633c586ad06660ca73e13062db425a46f00
NgoziEmeh <zingo1ng@yahoo.com>
2020-05-04 15:29:42 +0000

    scoreboard template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component'
+import { FooterComponent } from './footer/footer.component';
+import { ScoreboardComponent } from './scoreboard/scoreboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ScoreboardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/scoreboard/scoreboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-scoreboard',
+    templateUrl: './scoreboard.component.html'
+
+})
+export class ScoreboardComponent{
+
+}
