f58d75be764bac2db5de06960ea3186206e9a2d1
NgoziEmeh <zingo1ng@yahoo.com>
2020-05-04 19:37:47 +0000

    end style correct

--- src/app/endpage/endpage.component.ts
+++ src/app/endpage/endpage.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'app-endpage',
     templateUrl: './endpage.component.html',
-    styleUrls: ['./endpage.component,css']
+    styleUrls: ['./endpage.component.css']
 })
 export class EndpageComponent implements OnInit{
     constructor (){}
 
     ngOnInit(): void{
-        
+
     }
 
 }
