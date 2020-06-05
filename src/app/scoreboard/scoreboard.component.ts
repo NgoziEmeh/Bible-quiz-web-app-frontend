ae364119df3cdb3d05437505355e18ad9c0cc110
NgoziEmeh <zingo1ng@yahoo.com>
2020-06-05 10:17:07 +0000

    scoreboard doughnuts

--- src/app/scoreboard/scoreboard.component.ts
+++ src/app/scoreboard/scoreboard.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'app-scoreboard',
-    templateUrl: './scoreboard.component.html'
+    templateUrl: './scoreboard.component.html',
+    styleUrls: ['./scoreboard.component.css']
 
 })
 export class ScoreboardComponent{
